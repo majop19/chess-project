7cab31c9908fb73c93c59b273c3d486f0fafb3c6
majop19 <jp10ml2000@gmail.com>
2025-11-28 19:58:20 +0100

    test 7

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig, UserConfig } from "vite";
-import react from "@vitejs/plugin-react"
-import path from "path"
-import vike from "vike/plugin" with {type: "pointer"}
-import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import vike from "vike/plugin";
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 
 export default defineConfig({
@@ -31,4 +31,4 @@ export default defineConfig({
       },
     },
   },
-}) satisfies UserConfig;
+});
