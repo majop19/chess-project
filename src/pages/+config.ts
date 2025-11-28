e0ef7c166b60d73391cf94adf90c50f48654078a
majop19 <jp10ml2000@gmail.com>
2025-11-28 19:37:40 +0100

    test 3

--- src/pages/+config.ts
+++ src/pages/+config.ts
@@ -1,1 +1,9 @@
-export default {};
+import vikeReact from "vike-react/config";
+import { Config } from "vike/types";
+
+export default {
+  extends: vikeReact,
+  stream: "node",
+  passToClient: ["user", "loginAccessError"],
+  title: "chessApp Project",
+} satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { defineConfig, UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
