67c912e616faf4eea4306a2860b3191c0fef36d5
majop19 <jp10ml2000@gmail.com>
2025-11-28 18:35:19 +0100

    update config 2

--- src/pages/+config.ts
+++ src/pages/+config.ts
@@ -1,5 +1,10 @@
+import vikeReact from "vike-react/config";
+import { Config } from "vike/types";
+
 export default {
+  extends: vikeReact,
   stream: "node",
   passToClient: ["user", "loginAccessError"],
   title: "chessApp Project",
-};
+  _default: {},
+} satisfies Config;
