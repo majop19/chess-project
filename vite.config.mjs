0146b217548a329a21cff24684bce05d59eae686
majop19 <jp10ml2000@gmail.com>
2025-11-29 17:38:01 +0100

    test 10

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 
 export default defineConfig({
+  _default: false,
   plugins: [react(), vike(), tailwindcss()],
   resolve: {
     alias: [
