9fed2bc982555ab5f971122ddcebeb7a13c574ab
majop19 <jp10ml2000@gmail.com>
2025-12-03 19:54:20 +0100

    test 60

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import vike from "vike/plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { root } from "#back/root";
 
+const env = loadEnv(process.env.NODE_ENV || "development", process.cwd(), "");
 // https://vite.dev/config/
 export default defineConfig({
   root: process.env.NODE_ENV === "production" ? undefined : root,
   plugins: [react(), vike(), tailwindcss()],
   server: {
-    port: Number(process.env.PORT) || 3000,
+    port: Number(env.PORT) || 3000,
   },
   resolve: {
     alias: [
